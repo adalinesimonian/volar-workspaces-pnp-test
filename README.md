bda95673b3b5000902fa119fc204d08bcf130a35
Adaline Simonian <adaline.simonian@slickdeals.net>
2022-09-29 07:28:32 -0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# volar-workspaces-pnp-test
---
+++ packages/my-package/component.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="CustomButton" @click="onClick" :data-href="props.url">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { withDefaults, defineProps } from 'vue';
+
+interface Props {
+  url: string;
+}
+
+const props = withDefaults(defineProps<Props>(), { url: '/' });
+
+const onClick = () => {
+  window.location.assign(props.url);
+};
+</script>
+
+<style lang="css" scoped></style>
